27063bfd8d71d24c79a42543dc97d3059f1c0588
Angel Padilla <43829004+angeljadenpadilla@users.noreply.github.com>
2018-12-18 17:06:37 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,17 @@
+$(".start").click(function() {
+    $(".map").show();
+});
+$(".map").click(function() {
+    $(".cliff").show();
+   
+});
+$(".snap").click(function() {
+    $(".emo").fadeToggle();
+});
+$(".yeet").click(function() {
+    $(".gamora").slideToggle();
+});
+$(".arrr").dblclick(function() {
+    $(".stones").show();
+    $(".hide").fadeOut();
+});
